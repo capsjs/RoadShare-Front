517c39122ca148b4de808d1b7ed5868c728597c4
Manon <audibertmanon@hotmail.com>
2025-04-18 16:38:35 +0200

    Add configuration files for Babel, Metro, Tailwind CSS, and NativeWind
    - Created babel.config.js to set up Babel with Expo and NativeWind.
    - Added metro.config.js to configure Metro bundler with NativeWind support.
    - Introduced nativewind-env.d.ts for TypeScript support with NativeWind.
    - Established tailwind.config.js to define Tailwind CSS settings and custom font families.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import './globals.css';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -23,17 +26,15 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
+  if(!loaded) {
     return null;
   }
-
+ 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+        <StatusBar style="auto" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
