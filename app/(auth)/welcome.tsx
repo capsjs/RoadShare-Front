e07147d2ef838aaad63a511497a5bb1c11c22633
Manon <audibertmanon@hotmail.com>
2025-04-23 17:58:43 +0200

    feat: Add CustomButton component and related types
    - Implemented CustomButton component with customizable background and text variants.
    - Added constants for various images and icons used throughout the application.
    - Organized onboarding data structure for better management of onboarding screens.
    
    types: Define image and type declarations
    
    - Created type declarations for image imports to support various formats (png, jpg, jpeg, gif, svg).
    - Expanded type definitions for various interfaces including Driver, MarkerData, Ride, and more to improve type safety across the application.

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,16 @@
 import React, { useRef, useState } from 'react'
 import { router } from 'expo-router'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Swiper from 'react-native-swiper'
+
+import { onboarding } from '@/constants'
+import CustomButton from '@/components/customButton'
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
     <SafeAreaView className='flex h-full items-center justify-between'>
@@ -25,10 +29,31 @@ const Onboarding = () => {
         activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full"/>}
         onIndexChanged={(index) => setActiveIndex(index)}
       >
-        <View>
-          <Text>Enfant du swiper</Text>
-        </View>
+        {onboarding.map((item) => (
+          <View key={item.id} className='flex items-center justify-center p-5'>
+            <Image
+              source={item.image}
+              className='w-full h-[300px]'
+              resizeMode='contain'
+            />
+            <View className='flex items-center justify-center w-full mt-10'>
+              <Text className='text-black text-3xl font-bold mx-10 text-center'>
+                {item.title}
+              </Text>
+            </View>
+            <Text className='text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3'>
+              {item.description}
+            </Text>
+          </View>
+        ))}
       </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get started" : "Next"}
+        onPress={() => isLastSlide 
+          ? router.replace('/(auth)/signUp') 
+          : swiperRef.current?.scrollBy(1)}
+        className='w-11/12 mt-10'
+      />
     </SafeAreaView>
   )
 }
