eb7460d60eb50a5f4dd7eb2bd4ee1b9275232061
Manon <audibertmanon@hotmail.com>
2025-07-08 12:32:15 +0200

    feat: update expo-location dependency to version 18.1.6 and enhance Map component with improved styling and functionality

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps"
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { StyleSheet } from 'react-native';
+
 
 const Map = () => {
   return (
-   <MapView provider={PROVIDER_DEFAULT} className='h-full w-full rounded-2xl'
-   />    
-  )
-}
+    <MapView
+      provider={PROVIDER_DEFAULT} 
+      style={styles.map}
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+    </MapView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
