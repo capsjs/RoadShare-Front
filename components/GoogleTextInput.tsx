caaaa4c06789a89ca21aa12977fc64fc134cc23b
Manon <audibertmanon@hotmail.com>
2025-07-10 13:09:03 +0200

    feat: integrate Google Places Autocomplete and manage location state with Zustand

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,7 +1,13 @@
-import { View, Text } from 'react-native'
+import { Image, View } from 'react-native'
 import React from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Constants from 'expo-constants'; 
 
 import { GoogleInputProps } from '@/types/type'
+import { icons } from '@/constants';
+
+const googlePlacesApiKey = Constants.expoConfig?.extra?.EXPO_PUBLIC_GOOGLE_API_KEY;
+console.log(googlePlacesApiKey);
 
 const GoogleTextInput = ({
   icon,
@@ -10,11 +16,73 @@ const GoogleTextInput = ({
   textInputBackgroundColor,
   handlePress
 }: GoogleInputProps) => {
+
   return (
-    <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle}`}>
-      <Text>Search</Text>
+    <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl mb-5 ${containerStyle}`}>
+       {/* <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Search"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor
+              ? textInputBackgroundColor
+              : "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor
+              ? textInputBackgroundColor
+              : "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          },
+        }}
+        onPress={(data, details = null) => {
+          console.log('Selected data:', data);
+          console.log('Details:', details);
+          handlePress({
+            latitude: details?.geometry.location.lat!,
+            longitude: details?.geometry.location.lng!,
+            address: data.description,
+          });
+        }}
+        query={{
+          key: googlePlacesApiKey,
+          language: "fr",
+        }}
+        renderLeftButton={() => (
+          <View className="justify-center items-center w-6 h-6">
+            <Image
+              source={icon ? icon : icons.search}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </View>
+        )}
+        textInputProps={{
+          placeholderTextColor: "gray",
+          placeholder: initialLocation ?? "Où va-t-on?",
+        }}
+      /> */}
     </View>
   )
-}
+};
 
 export default GoogleTextInput
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-import { LocationStore } from "@/types/type";
+import { DriverStore, LocationStore } from "@/types/type";
 
 export const useLocationStore = create<LocationStore>((set) => ({
   userAddress: null,
